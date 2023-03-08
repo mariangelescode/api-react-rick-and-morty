3cda860113be2da0318284d894d76341b44e579f
Mariangeles <mari_riveraper@live.com.mx>
2023-03-07 21:44:59 -0600

    new style

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -4,7 +4,7 @@ export default function Characters( {name, img, status} : {name: string, img: st
     return(
         <div className='container__character'>
             <img className={`img__character ${status == 'Alive' ? 'alive' : 'dead'}`} src={img} alt={name} />
-            <div className='container__name'>{name}</div>
+            <div className='container__name names'>{name}</div>
         </div>
     )
 }
